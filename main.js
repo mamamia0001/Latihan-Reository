e99200bd7c851ac281b186ab817337b88c252a5b
Mahendra <tumbalakun827@gmail.com>
2024-04-17 12:01:49 +0800

    ver 1

---
+++ main.js
@@ -0,0 +1,41 @@
+//alert('halo');
+
+//console.log('halo')
+
+//prompt('masukan usia anda')
+
+//let nama = 'Gde Beefolk'
+//let usia = 17
+//let pacar = null
+//console.log('Halo, usia kamu ' + usia, 'nama saya ' + nama)
+
+//'nama saya ' +nama,
+//'tinggi badan saya ' +tinggiBadan,
+//'hobi saya ' +hobi,
+//'saya lahir di ' +tempatLahir.kota,
+// alert (`nama saya ${nama} usia saya ${usia}`)
+
+//let saldoAwal = 5000
+//let saldoTambahan = 1000
+//const hutang = 3000
+//const saldoAkhir = saldoAwal + saldoTambahan - hutang
+
+//alert(`nama saya ${nama} usia saya ${usia}`)
+//alert(`saya memiliki saldo Rp ${saldoAwal}`)
+
+//ini array
+let namaMahasiswa = ['Mie', 'Shie', 'Chao']
+namaMahasiswa.push('Makan', 'Mie')
+namaMahasiswa.shift()
+namaMahasiswa.pop()
+alert (namaMahasiswa)[1],[2]
+
+//ini for loop
+for (let i = 1; i <= 10; i = i + 1){
+    console.log('dasar javascript')
+}
+
+const namaMahasiswa=['Mie', 'Shie', 'Chao']
+for (let i = 0; i < namaMahasiswa.length ;i++){
+    
+}
